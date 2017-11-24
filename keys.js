346e51384976e90f53557d232cc3c355fd2d2ba4
JakeL501 <jacob.letourneau@my.colby-sawyer.edu>
2017-11-24 16:26:11 -0500

    packages, keys, main code, issues with spotify and omdb code, twitter functional

---
+++ keys.js
@@ -0,0 +1,20 @@
+console.log('this is loaded');
+
+// Twitter
+var twitterKeys = {
+  consumer_key: 'c4pKVUzrsfwYZ0f5tA1aLvlL3',
+  consumer_secret: 'YjuL5TXMs3iYS0n1NMMarVxEaClIJ66bAMM1DiqyeXImuQEivK',
+  access_token_key: '933109213471133696-HpkLUqMwIbmdj14Vw63eQiDSqkmvIbA',
+  access_token_secret: 'xd44uRFOuDoJoloPa1uJKnpoI1ZMPQYVIrG3pybH6SS2L'
+}
+// Spotify
+var spotifyKeys = {
+  client_ID: '1643458b064a4012b0e6e486818e1465',
+  client_secret: '0bc0624449094d2b9664ca16eacde3f3'
+}
+// OMDB
+var omdbKey = 'b01d63b6';
+
+module.exports.twitterKeys = twitterKeys;
+module.exports.spotifyKeys = spotifyKeys;
+module.exports.omdbKey = omdbKey;
